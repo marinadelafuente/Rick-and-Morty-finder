5ab834a82e53947d556676fdfb1b8acee87effd2
marinadelafuente <mdelafuente18@gmail.com>
2020-01-13 08:28:27 +0100

    add docs

---
+++ docs/precache-manifest.020ccdbbefc560c12a325d44165fa889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5df32c1ffb21e5c35d5a983d1a5f459",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c509b0be9ed68e0e604",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "656d9e16e1c3ff807258",
+    "url": "./static/js/2.063e8f50.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.063e8f50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c509b0be9ed68e0e604",
+    "url": "./static/js/main.bcac999e.chunk.js"
+  },
+  {
+    "revision": "78ce75fc3061d2b87b84",
+    "url": "./static/js/runtime-main.23e88e8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
